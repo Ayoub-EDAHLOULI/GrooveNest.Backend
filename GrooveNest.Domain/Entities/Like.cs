e79b79c063c5ec8d96b393bd1e16eeeed4ab0217
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-19 23:25:00 +0100

    Add Like class to manage user track interactions
    Introduces the `Like` class in the `GrooveNest.Domain.Entities` namespace. This class features properties for `Id`, `CreatedAt`, `TrackId`, `Track`, `UserId`, and `User`, facilitating relationships with `Track` and `User` entities. The `CreatedAt` property is set to the current UTC date and time.

---
+++ GrooveNest.Domain/Entities/Like.cs
@@ -0,0 +1,18 @@
+﻿namespace GrooveNest.Domain.Entities
+{
+    public class Like
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+        // --------------------------------------------------- //
+        // ---------------- Relationship Area ---------------- //
+        // --------------------------------------------------- //
+        public int TrackId { get; set; }
+        public Track Track { get; set; } = null!;
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
