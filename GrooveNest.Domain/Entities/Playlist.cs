7692d21cb742cf64ffb8ba70c293430c9e956735
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-19 23:20:02 +0100

    Add Playlist class to Domain.Entities namespace
    Introduces a new `Playlist` class with properties for
    `Id`, `Name`, `IsPublic`, and `CreatedAt`. Establishes
    a relationship to the `User` class via `OwnerId` and
    `User` properties, with `CreatedAt` defaulting to the
    current UTC date and time.

---
+++ GrooveNest.Domain/Entities/Playlist.cs
@@ -0,0 +1,17 @@
+﻿namespace GrooveNest.Domain.Entities
+{
+    public class Playlist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public bool IsPublic { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+        // --------------------------------------------------- //
+        // ---------------- Relationship Area ---------------- //
+        // --------------------------------------------------- //
+        public Guid OwnerId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
