2d6dc5e3c9bd57a1e056bc3acee6ea1ee64b23de
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-19 23:26:30 +0100

    Add Comment class to manage track comments
    Introduces the `Comment` class in the `GrooveNest.Domain.Entities` namespace. This class includes properties for `Id`, `Content`, `CreatedAt`, `TrackId`, `Track`, `UserId`, and `User`, facilitating the association of comments with tracks and users in the application.

---
+++ GrooveNest.Domain/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿namespace GrooveNest.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+        // --------------------------------------------------- //
+        // ---------------- Relationship Area ---------------- //
+        // --------------------------------------------------- //
+
+        public int TrackId { get; set; }
+        public Track Track { get; set; } = null!;
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
