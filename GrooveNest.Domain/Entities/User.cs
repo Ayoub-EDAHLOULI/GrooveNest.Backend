84c98f02fbcd9c27bd29abed620051f13b337564
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-19 22:49:06 +0100

    Add User class
    Introduced a new `User` class in `User.cs` to represent core account records for listeners, artists, and admins, with properties for `Id`, `UserName`, `Email`, `Password`, and `CreateAt`.

---
+++ GrooveNest.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace GrooveNest.Domain.Entities
+{
+    /// <summary>
+    /// Core account record for listeners, artists, and admins.
+    /// </summary>
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+    }
+}
