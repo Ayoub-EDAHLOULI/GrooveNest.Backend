3a5cbd2eaf0f55726e81ffedebe8a98201d869e9
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-19 23:11:57 +0100

    Add Track class to manage audio track details
    Introduced a new `Track` class in the `GrooveNest.Domain.Entities` namespace. The class includes properties for `Id`, `Title`, `DurationSec`, `AudioUrl`, `TrackNumber`, and `AlbumId`, and establishes a relationship with the `Album` class.

---
+++ GrooveNest.Domain/Entities/Track.cs
@@ -0,0 +1,18 @@
+﻿namespace GrooveNest.Domain.Entities
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int DurationSec { get; set; }
+        public string AudioUrl { get; set; } = null!;
+        public int TrackNumber { get; set; }
+
+
+        // --------------------------------------------------- //
+        // ---------------- Relationship Area ---------------- //
+        // --------------------------------------------------- //
+        public int AlbumId { get; set; }
+        public Album Album { get; set; } = null!;
+    }
+}
