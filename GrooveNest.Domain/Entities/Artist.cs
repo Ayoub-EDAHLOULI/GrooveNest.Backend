e86963c991921ab1d4538de56c012ae51ecd0905
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-19 23:05:40 +0100

    Add Artist class to GrooveNest.Domain.Entities
    Introduced a new `Artist` class with properties for `Id`, `Name`, `Bio`, and `AvatarUrl`. Added nullable `UserId` and `User` reference to establish a relationship with the `User` entity.

---
+++ GrooveNest.Domain/Entities/Artist.cs
@@ -0,0 +1,17 @@
+﻿namespace GrooveNest.Domain.Entities
+{
+    public class Artist
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = null!;
+        public string? Bio { get; set; }
+        public string? AvatarUrl { get; set; }
+
+
+        // --------------------------------------------------- //
+        // ---------------- Relationship Area ---------------- //
+        // --------------------------------------------------- //
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
