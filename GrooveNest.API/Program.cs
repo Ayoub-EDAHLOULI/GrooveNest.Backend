0748fabaa03cfe7800679957506aa5d2b171ad34
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-21 23:46:52 +0100

    Add user secrets and EF Core setup in API project
    Updated `GrooveNest.API.csproj` to include a `UserSecretsId` and added a reference to `Microsoft.EntityFrameworkCore.Design`. Added project references to other GrooveNest components.
    
    In `Program.cs`, added using directives for `GrooveNest.Repository.Data` and `Microsoft.EntityFrameworkCore`. Configured `AppDbContext` to use SQL Server with a connection string from configuration while retaining existing service registrations for controllers and Swagger/OpenAPI.

--- GrooveNest.API/Program.cs
+++ GrooveNest.API/Program.cs
@@ -1,7 +1,15 @@
+using GrooveNest.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Retrieve the connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
