24be48adb5ddc7fffcd7bc5016bd8800abb05490
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-21 22:41:06 +0100

    Add Entity Framework Core setup and AppDbContext
    Updated `GrooveNest.Repository.csproj` to include Entity Framework Core (v9.0.5) package references and added a project reference to `GrooveNest.Domain`.
    
    Created `AppDbContext.cs` to define the database context with `DbSet` properties for various entities, enabling data access functionality.

---
+++ GrooveNest.Repository/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using GrooveNest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrooveNest.Repository.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<TrackGenre> TrackGenres { get; set; }
+        public DbSet<PlaylistTrack> PlaylistTracks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+
+    }
+}
