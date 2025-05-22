de7c401f5e4d9d659dd521821f96adceafe93d5a
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-22 23:51:35 +0100

    Refactor project structure and add UserController
    Introduced a new UserController class in UserController.cs,
    configured as an API controller with routing attributes.
    Note: Constructor syntax needs correction from parentheses
    to colon for proper dependency injection.

---
+++ GrooveNest.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using GrooveNest.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooveNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(UserService userService) : ControllerBase
+    {
+        private readonly UserService _userService = userService;
+    }
+}
