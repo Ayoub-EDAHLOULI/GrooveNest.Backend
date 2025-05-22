fc68b383e62afd2a22975f4eaf4585f6b9e59dd7
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-22 22:21:34 +0100

    Add IGenericRepository interface for generic operations
    Introduces the `IGenericRepository<T, TKey>` interface in the `GrooveNest.Repository.Interfaces` namespace. This interface implements a generic repository pattern with asynchronous methods for retrieving, adding, updating, and deleting entities, supporting any class type `T` and key type `TKey`.

---
+++ GrooveNest.Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace GrooveNest.Repository.Interfaces
+{
+    public interface IGenericRepository<T, TKey> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(TKey id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(TKey id);
+    }
+}
