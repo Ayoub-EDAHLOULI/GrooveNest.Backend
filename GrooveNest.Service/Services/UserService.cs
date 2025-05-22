753ad903f795f0b35f704c5c4524e680783401e3
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-22 23:03:58 +0100

    Refactor UserService to implement IUserService interface
    Updated UserService to implement IUserService. Constructor now accepts IUserRepository. Added several async methods with NotImplementedException. Updated namespace with additional using directives.

--- GrooveNest.Service/Services/UserService.cs
+++ GrooveNest.Service/Services/UserService.cs
@@ -1,6 +1,42 @@
-﻿namespace GrooveNest.Service.Services
+﻿using GrooveNest.Domain.DTOs.UserDTOs;
+using GrooveNest.Repository.Interfaces;
+using GrooveNest.Service.Interfaces;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Services
 {
-    public class UserService
+    public class UserService(IUserRepository userRepository) : IUserService
     {
+        private readonly IUserRepository _userRepository = userRepository;
+
+        public Task<ApiResponse<UserResponseDto>> CreateUserAsync(UserCreateDto userCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> DeleteUserAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<object>> GetAllPaginatedUsersAsync(int page = 1, int pageSize = 10, string searchQuery = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<List<UserResponseDto>>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<UserResponseDto>> GetUserByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<UserResponseDto>> UpdateUserAsync(Guid id, UserUpdateDto userUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
