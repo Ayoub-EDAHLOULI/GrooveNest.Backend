d3c56a56d25f6b8eadfd0a0ffb98339816981e96
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-22 23:53:31 +0100

    Add GetAllUsersAsync method to UserController
    Implemented an asynchronous HTTP GET endpoint in the UserController to retrieve all users. The method returns a NotFound response if the retrieval fails, or an Ok response with user data if successful. Added comments for better code readability and organization.

--- GrooveNest.API/Controllers/UserController.cs
+++ GrooveNest.API/Controllers/UserController.cs
@@ -8,5 +8,21 @@ namespace GrooveNest.API.Controllers
     public class UserController(UserService userService) : ControllerBase
     {
         private readonly UserService _userService = userService;
+
+
+        // ------------------------------------------------------------------------- //
+        // ------------------------ GetAllUsersAsync METHODS ----------------------- //
+        // ------------------------------------------------------------------------- // 
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllUsersAsync()
+        {
+            var response = await _userService.GetAllUsersAsync();
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
     }
 }
