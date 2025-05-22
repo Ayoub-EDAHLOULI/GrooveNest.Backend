36120a7a1a2f1f5a26e90242bcabc585f8e6f69f
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-22 22:31:35 +0100

    Refactor repository structure and update interface methods
    - Removed folder reference for "Repositories" in the project file.
    - Updated `IGenericRepository` interface methods to accept entities directly for `AddAsync`, `UpdateAsync`, and `DeleteAsync`.
    - Introduced `GenericRepository` class implementing `IGenericRepository`, providing asynchronous methods for database operations using Entity Framework Core.

--- GrooveNest.Repository/Interfaces/IGenericRepository.cs
+++ GrooveNest.Repository/Interfaces/IGenericRepository.cs
@@ -4,8 +4,8 @@
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T?> GetByIdAsync(TKey id);
-        Task<T> AddAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<bool> DeleteAsync(TKey id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
---
+++ GrooveNest.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using GrooveNest.Repository.Data;
+using GrooveNest.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrooveNest.Repository.Repositories
+{
+    public class GenericRepository<T, TKey>(AppDbContext context) : IGenericRepository<T, TKey> where T : class
+    {
+        private readonly AppDbContext _context = context;
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(TKey id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
