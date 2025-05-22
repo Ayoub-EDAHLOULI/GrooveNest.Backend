69c896efd8f8cddf2ef7f4d5ec8189e5c04053e1
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-22 22:37:47 +0100

    Add UserCreateDto class for user registration
    Introduced a new `UserCreateDto` class in the
    `GrooveNest.Domain.DTOs.UserDTOs` namespace. This class
    includes three properties: `UserName`, `Email`, and
    `Password`, all of which are required and initialized
    to `null!`.

---
+++ GrooveNest.Domain/DTOs/UserDTOs/UserCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GrooveNest.Domain.DTOs.UserDTOs
+{
+    public class UserCreateDto
+    {
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
