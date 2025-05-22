b89f9c0908ae8d5c8f74fb1f5a3017ce9280fec7
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-22 22:36:59 +0100

    Add UserResponseDto class for user data representation
    Introduced a new `UserResponseDto` class in the
    `GrooveNest.Domain.DTOs.UserDTOs` namespace. This class
    includes properties for `Id`, `UserName`, `Email`, and
    `CreatedAt`, with `CreatedAt` defaulting to the current
    UTC time.

---
+++ GrooveNest.Domain/DTOs/UserDTOs/UserResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GrooveNest.Domain.DTOs.UserDTOs
+{
+    public class UserResponseDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
