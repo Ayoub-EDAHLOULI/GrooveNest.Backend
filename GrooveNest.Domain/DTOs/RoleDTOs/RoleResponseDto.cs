59b181c7fbbb1025588fe8ce3f00e0888b441015
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-23 22:07:42 +0100

    Add RoleResponseDto class for role data transfer
    Introduces the `RoleResponseDto` class in the
    `GrooveNest.Domain.DTOs.RoleDTOs` namespace.
    This class includes two properties: `Id` (int)
    and `Name` (string, non-nullable).

---
+++ GrooveNest.Domain/DTOs/RoleDTOs/RoleResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GrooveNest.Domain.DTOs.RoleDTOs
+{
+    public class RoleResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
