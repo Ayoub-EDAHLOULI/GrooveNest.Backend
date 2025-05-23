d56de5a1c59deffc3aa67f74637a886ba1f53108
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-23 22:39:45 +0100

    Add RoleController to manage role-related operations
    Introduces a new `RoleController` class in the `GrooveNest.API.Controllers` namespace. Adds using directives for `GrooveNest.Service.Services` and `Microsoft.AspNetCore.Mvc`. The class is set up with routing and API controller attributes, and includes a constructor that initializes a private readonly `RoleService` field.

---
+++ GrooveNest.API/Controllers/RoleController.cs
@@ -0,0 +1,12 @@
+﻿using GrooveNest.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooveNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController(RoleService roleService) : ControllerBase
+    {
+        private readonly RoleService _roleService = roleService;
+    }
+}
