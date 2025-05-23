6b88bdff03ef6503b5048d928fdc94837fd30a49
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-23 23:21:19 +0100

    Add IUserRoleService interface for user role management
    Introduces the `IUserRoleService` interface in the
    `GrooveNest.Service.Interfaces` namespace. This interface
    defines four asynchronous methods: `GetAllUserRoleAsync`,
    `GetUserRoleByIdAsync`, `AddUserRoleAsync`, and
    `DeleteUserRoleAsync`, each returning an `ApiResponse`
    with user role data. Also includes necessary using
    directives for `UserRoleDTOs` and `Utilities`.

---
+++ GrooveNest.Service/Interfaces/IUserRoleService.cs
@@ -0,0 +1,13 @@
+﻿using GrooveNest.Domain.DTOs.UserRoleDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface IUserRoleService
+    {
+        Task<ApiResponse<IList<UserRoleResponseDto>>> GetAllUserRoleAsync();
+        Task<ApiResponse<UserRoleResponseDto>> GetUserRoleByIdAsync(Guid userId, int roleId);
+        Task<ApiResponse<UserRoleResponseDto>> AddUserRoleAsync(UserRoleCreateDto userRoleCreateDto);
+        Task<ApiResponse<UserRoleResponseDto>> DeleteUserRoleAsync(Guid userId, int roleId);
+    }
+}
