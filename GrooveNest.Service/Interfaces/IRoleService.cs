7ab7ff4283dfcfea657d1f6f910794d8c63533a4
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-23 22:16:31 +0100

    Add IRoleService interface and RoleService implementation
    Updated IRoleService.cs to define an interface for role management with asynchronous methods for retrieving, creating, updating, and deleting roles. Added necessary DTOs and utility namespaces.
    
    Modified RoleService.cs to implement the IRoleService interface, correcting a syntax error in the class declaration and initializing a private readonly field for the role repository.

---
+++ GrooveNest.Service/Interfaces/IRoleService.cs
@@ -0,0 +1,16 @@
+﻿using GrooveNest.Domain.DTOs.RoleDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface IRoleService
+    {
+        Task<ApiResponse<List<RoleResponseDto>>> GetAllRolesAsync();
+        Task<ApiResponse<object>> GetAllPaginatedRolesAsync(int page = 1, int pageSize = 10, string searchQuery = "");
+        Task<ApiResponse<RoleResponseDto>> GetRoleByIdAsync(int id);
+        Task<ApiResponse<RoleResponseDto>> CreateRoleAsync(RoleCreateDto roleCreateDto);
+        Task<ApiResponse<RoleResponseDto>> UpdateRoleAsync(int id, RoleUpdateDto roleUpdateDto);
+        Task<ApiResponse<string>> DeleteRoleAsync(int id);
+
+    }
+}
---
+++ GrooveNest.Service/Services/RoleService.cs
@@ -0,0 +1,9 @@
+﻿using GrooveNest.Repository.Interfaces;
+
+namespace GrooveNest.Service.Services
+{
+    public class RoleService(IRoleRepository roleRepository)
+    {
+        private readonly IRoleRepository _roleRepository = roleRepository;
+    }
+}
