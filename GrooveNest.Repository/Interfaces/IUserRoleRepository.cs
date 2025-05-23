834914d2aedc70d4851bf66cd32a18451345f7cb
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-23 22:55:29 +0100

    Add IUserRoleRepository interface for role management
    Introduces the `IUserRoleRepository` interface in the
    `GrooveNest.Repository.Interfaces` namespace. This interface
    extends a generic repository for the `UserRole` entity and
    includes three asynchronous methods: `GetByIdsAsync`,
    `GetRolesByUserIdAsync`, and `GetUsersByRoleIdAsync` for
    managing user roles and their associations.

---
+++ GrooveNest.Repository/Interfaces/IUserRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface IUserRoleRepository : IGenericRepository<UserRole, Guid>
+    {
+        Task<UserRole?> GetByIdsAsync(Guid userId, int roleId);
+        Task<IEnumerable<Role>> GetRolesByUserIdAsync(Guid userId);
+        Task<IEnumerable<User>> GetUsersByRoleIdAsync(int roleId);
+    }
+}
