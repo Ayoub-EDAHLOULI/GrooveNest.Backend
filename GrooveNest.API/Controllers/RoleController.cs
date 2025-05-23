34b843fc87b66b3d4537d73880c4cb4753564280
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-23 22:40:25 +0100

    Add GetAllRolesAsync method to RoleController
    Updated the RoleController class to include a new asynchronous HTTP GET method, `GetAllRolesAsync`, which retrieves all roles from the RoleService. The method handles success and failure responses appropriately and includes comments for better code organization and readability.

--- GrooveNest.API/Controllers/RoleController.cs
+++ GrooveNest.API/Controllers/RoleController.cs
@@ -8,5 +8,21 @@ namespace GrooveNest.API.Controllers
     public class RoleController(RoleService roleService) : ControllerBase
     {
         private readonly RoleService _roleService = roleService;
+
+
+        // ------------------------------------------------------------------------- //
+        // ------------------------ GetAllRolesAsync METHODS ----------------------- //
+        // ------------------------------------------------------------------------- // 
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllRolesAsync()
+        {
+            var response = await _roleService.GetAllRolesAsync();
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
