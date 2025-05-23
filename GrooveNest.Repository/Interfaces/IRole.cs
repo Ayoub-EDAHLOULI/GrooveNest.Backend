8f4af204985576727521c7bccf01726fef3b27e0
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-23 22:11:19 +0100

    Add IRole interface for role management
    Introduces the IRole interface in the GrooveNest.Repository.Interfaces namespace, extending IGenericRepository<Role, int>. Adds the GetByNameAsync method for asynchronous retrieval of roles by name, returning a Task<Role?>. Includes necessary using directive for GrooveNest.Domain.Entities.

---
+++ GrooveNest.Repository/Interfaces/IRole.cs
@@ -0,0 +1,14 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface IRole : IGenericRepository<Role, int>
+    {
+        /// <summary>
+        /// Get a role by its name.
+        /// </summary>
+        /// <param name="name">The name of the role.</param>
+        /// <returns>The role with the specified name, or null if not found.</returns>
+        Task<Role?> GetByNameAsync(string name);
+    }
+}
