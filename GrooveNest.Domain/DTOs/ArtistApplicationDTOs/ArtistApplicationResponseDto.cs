620d1aafc85395e7f3db2763d00278b1a3062475
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-24 22:27:17 +0100

    Add ArtistApplicationResponseDto class
    Introduces the ArtistApplicationResponseDto class in the GrooveNest.Domain.DTOs.ArtistApplicationDTOs namespace. This class contains properties for Id, UserId, UserName, Message, SubmittedAt, and IsApproved to represent artist application response data.

---
+++ GrooveNest.Domain/DTOs/ArtistApplicationDTOs/ArtistApplicationResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GrooveNest.Domain.DTOs.ArtistApplicationDTOs
+{
+    public class ArtistApplicationResponseDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string? Message { get; set; }
+        public DateTime SubmittedAt { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
