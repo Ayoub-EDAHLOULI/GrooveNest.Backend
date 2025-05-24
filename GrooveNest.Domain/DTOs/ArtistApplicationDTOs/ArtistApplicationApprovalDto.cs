f3517edf422b01151500e5b903a6624b47360a40
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-24 22:28:26 +0100

    Create artistapplicationapprovaldto this is used by the admin to approve or reject the application.

---
+++ GrooveNest.Domain/DTOs/ArtistApplicationDTOs/ArtistApplicationApprovalDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GrooveNest.Domain.DTOs.ArtistApplicationDTOs
+{
+    public class ArtistApplicationApprovalDto
+    {
+        public Guid ApplicationId { get; set; }
+        public bool Approve { get; set; }
+    }
+}
