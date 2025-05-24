d4064db111d04b8f0950e9e317b8146416298d7b
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-24 22:09:12 +0100

    Create ArtistApplication entity in case some users want to become an artist you will send a request to the admin

---
+++ GrooveNest.Domain/Entities/ArtistApplication.cs
@@ -0,0 +1,11 @@
+﻿namespace GrooveNest.Domain.Entities
+{
+    public class ArtistApplication
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool IsApproved { get; set; } = false;
+        public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
+    }
+}
