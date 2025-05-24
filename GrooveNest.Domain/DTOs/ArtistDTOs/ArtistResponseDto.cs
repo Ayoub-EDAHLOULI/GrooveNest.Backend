d35066f8079b631c7e356b1df4555fc2c2643fb8
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-24 12:49:13 +0100

    Add ArtistResponseDto class for artist data transfer
    Introduced a new `ArtistResponseDto` class in the
    `GrooveNest.Domain.DTOs.ArtistDTOs` namespace. This class
    includes properties for `Id`, `Name`, `Bio`, and `UserName`,
    with appropriate data types and nullability settings.

---
+++ GrooveNest.Domain/DTOs/ArtistDTOs/ArtistResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GrooveNest.Domain.DTOs.ArtistDTOs
+{
+    public class ArtistResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Bio { get; set; }
+        public string? UserName { get; set; }
+    }
+}
