06b7e699e6d26c4e059c487b67330741a5e44ea8
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-24 11:33:42 +0100

    Add user role management services and controller
    This commit introduces new service registrations for user role management in `Program.cs`, including `UserRoleRepository`, `IUserRoleRepository`, `UserRoleService`, and `IUserRoleService`. Additionally, a new `UserRoleController` is created to handle HTTP requests related to user roles, complete with routing and API controller attributes.

---
+++ GrooveNest.API/Controllers/UserRoleController.cs
@@ -0,0 +1,12 @@
+﻿using GrooveNest.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooveNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRoleController(UserRoleService userRoleService) : ControllerBase
+    {
+        private readonly UserRoleService _userRoleService = userRoleService;
+    }
+}
--- GrooveNest.API/Program.cs
+++ GrooveNest.API/Program.cs
@@ -25,18 +25,22 @@ builder.Services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericReposito
 
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<RoleRepository>();
+builder.Services.AddScoped<UserRoleRepository>();
 
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IUserRoleRepository, UserRoleRepository>();
 
 
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<RoleService>();
+builder.Services.AddScoped<UserRoleService>();
 
 
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IUserRoleService, UserRoleService>();
 
 
 
