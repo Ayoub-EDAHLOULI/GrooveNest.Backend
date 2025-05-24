b41610d7ed4bf573e156ba13b60e61b08e35d2f6
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-24 12:27:22 +0100

    Add ArtistUpdateDto class for artist information updates
    Introduces the `ArtistUpdateDto` class in the
    `GrooveNest.Domain.DTOs.ArtistDTOs` namespace. This class
    includes properties for `Id`, `Name`, `Bio`, and `AvatarUrl`,
    facilitating updates to artist information in the application.

---
+++ GrooveNest.Domain/DTOs/ArtistDTOs/ArtistUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GrooveNest.Domain.DTOs.ArtistDTOs
+{
+    public class ArtistUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Bio { get; set; }
+        public string? AvatarUrl { get; set; }
+    }
+}
