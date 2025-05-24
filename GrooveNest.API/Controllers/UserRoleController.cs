1e1f9b9c09351c434b18edc987a1066c4c38a901
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-24 11:34:33 +0100

    Add GetAllUserRolesAsync method to UserRoleController
    Implemented an asynchronous HTTP GET endpoint in the UserRoleController to retrieve all user roles. The method calls GetAllUserRoleAsync from UserRoleService and handles success and failure responses appropriately. Added comments for better context.

--- GrooveNest.API/Controllers/UserRoleController.cs
+++ GrooveNest.API/Controllers/UserRoleController.cs
@@ -8,5 +8,21 @@ namespace GrooveNest.API.Controllers
     public class UserRoleController(UserRoleService userRoleService) : ControllerBase
     {
         private readonly UserRoleService _userRoleService = userRoleService;
+
+
+        // ---------------------------------------------------------------------------- //
+        // ------------------------ GetAllUserRoleAsync METHODS ----------------------- //
+        // ---------------------------------------------------------------------------- // 
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUserRolesAsync()
+        {
+            var response = await _userRoleService.GetAllUserRoleAsync();
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
