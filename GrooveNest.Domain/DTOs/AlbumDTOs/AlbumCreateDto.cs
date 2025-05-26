dd3751895afc3dea294fc84db0fe4c07b298a1f6
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 20:42:12 +0100

    Add AlbumCreateDto class for album creation
    Introduces the AlbumCreateDto class in the
    GrooveNest.Domain.DTOs.AlbumDTOs namespace.
    This class includes properties for Title,
    ReleaseDate, ArtistId, and an optional Cover
    of type IFormFile to facilitate file uploads.

---
+++ GrooveNest.Domain/DTOs/AlbumDTOs/AlbumCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GrooveNest.Domain.DTOs.AlbumDTOs
+{
+    public class AlbumCreateDto
+    {
+        public string Title { get; set; } = null!;
+        public DateTime ReleaseDate { get; set; }
+        public Guid ArtistId { get; set; }
+        public IFormFile? Cover { get; set; }
+    }
+}
