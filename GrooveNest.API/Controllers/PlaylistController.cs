bc4ebc16596c8b4be3e9ab372727ac3738790f81
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 23:02:47 +0100

    Add PlaylistController to manage playlist operations
    Introduces a new `PlaylistController` class in the
    `GrooveNest.API.Controllers` namespace. Adds using
    directives for necessary services and decorates the
    class with routing and API controller attributes.
    Includes a constructor to accept and store a
    `PlaylistService` instance for handling HTTP requests.

---
+++ GrooveNest.API/Controllers/PlaylistController.cs
@@ -0,0 +1,12 @@
+﻿using GrooveNest.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooveNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController(PlaylistService playlistService) : ControllerBase
+    {
+        private readonly PlaylistService _playlistService = playlistService;
+    }
+}
