04d6f69cf18c7a2d86e93948602858e7c605b117
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 20:43:42 +0100

    Add AlbumUpdateDto class for album updates
    Introduced the `AlbumUpdateDto` class in the
    `GrooveNest.Domain.DTOs.AlbumDTOs` namespace. This
    class features properties for `Title`, `ReleaseDate`,
    and `Cover` to facilitate album updates and file
    uploads in ASP.NET Core.

---
+++ GrooveNest.Domain/DTOs/AlbumDTOs/AlbumUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GrooveNest.Domain.DTOs.AlbumDTOs
+{
+    public class AlbumUpdateDto
+    {
+        public string? Title { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public IFormFile? Cover { get; set; }
+    }
+}
