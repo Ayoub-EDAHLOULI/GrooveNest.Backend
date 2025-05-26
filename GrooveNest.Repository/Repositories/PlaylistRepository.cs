4da93ef74f41f61cd4c3d26664adfc7eb40ab5ae
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 22:43:53 +0100

    Add PlaylistRepository with GetPlaylistByName method
    Introduces a new `PlaylistRepository` class that extends
    `GenericRepository<Playlist, Guid>` and implements the
    `IPlaylistRepository` interface. Adds a method to retrieve
    playlists by name, including the owner. Necessary namespaces
    for entities, data, and interfaces are also included.

---
+++ GrooveNest.Repository/Repositories/PlaylistRepository.cs
@@ -0,0 +1,17 @@
+﻿using GrooveNest.Domain.Entities;
+using GrooveNest.Repository.Data;
+using GrooveNest.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrooveNest.Repository.Repositories
+{
+    public class PlaylistRepository(AppDbContext context) : GenericRepository<Playlist, Guid>(context), IPlaylistRepository
+    {
+        public async Task<Playlist?> GetPlaylistByName(string name)
+        {
+            return await _context.Playlists
+                .Include(p => p.Owner)
+                .FirstOrDefaultAsync(p => p.Name == name);
+        }
+    }
+}
