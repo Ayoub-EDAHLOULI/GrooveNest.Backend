0ad3ca3a03345ceb1e917fea48f289db1d32cdf2
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 22:34:59 +0100

    Add PlaylistCreateDto class for playlist creation
    Introduces the `PlaylistCreateDto` class in the
    `GrooveNest.Domain.DTOs.PlaylistDTOs` namespace.
    This class features three properties: `Name` (non-nullable
    string), `IsPublic` (boolean with a default value of true),
    and `OwnerId` (GUID for the playlist owner).

---
+++ GrooveNest.Domain/DTOs/PlaylistDTOs/PlaylistCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GrooveNest.Domain.DTOs.PlaylistDTOs
+{
+    public class PlaylistCreateDto
+    {
+        public string Name { get; set; } = null!;
+        public bool IsPublic { get; set; } = true;
+        public Guid OwnerId { get; set; }
+    }
+}
