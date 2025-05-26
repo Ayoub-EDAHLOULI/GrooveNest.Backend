04e6c5e2e9c47c66d8f3e08b7dd75f2933318cb8
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 22:46:53 +0100

    Add IPlaylistService interface for playlist management
    Introduces the `IPlaylistService` interface in the
    `GrooveNest.Service.Interfaces` namespace. This interface
    defines asynchronous methods for managing playlists,
    including retrieval, creation, updating, and deletion.
    Each method returns an `ApiResponse` with appropriate
    data types, enhancing the service layer for playlist
    operations.

---
+++ GrooveNest.Service/Interfaces/IPlaylistService.cs
@@ -0,0 +1,15 @@
+﻿using GrooveNest.Domain.DTOs.PlaylistDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface IPlaylistService
+    {
+        Task<ApiResponse<List<PlaylistResponseDto>>> GetAllPlaylistsAsync();
+        Task<ApiResponse<object>> GetAllPaginatedPlaylistsAsync(int page = 1, int pageSize = 10, string searchQuery = "");
+        Task<ApiResponse<PlaylistResponseDto>> GetPlaylistByIdAsync(Guid id);
+        Task<ApiResponse<PlaylistResponseDto>> CreatePlaylistAsync(PlaylistCreateDto playlistCreateDto);
+        Task<ApiResponse<PlaylistResponseDto>> UpdatePlaylistAsync(Guid id, PlaylistUpdateDto playlistUpdateDto);
+        Task<ApiResponse<string>> DeletePlaylistAsync(Guid id);
+    }
+}
