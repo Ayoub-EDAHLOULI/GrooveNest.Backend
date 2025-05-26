d459525964047f104746b8c0ce2183070935ed09
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 22:39:44 +0100

    Add PlaylistResponseDto class for playlist details
    Introduces the `PlaylistResponseDto` class in the
    `GrooveNest.Domain.DTOs.PlaylistDTOs` namespace. This
    class contains properties for `Id`, `Name`, `IsPublic`,
    `CreatedAt`, `OwnerId`, and `OwnerUserName`, with
    default values for `Name` and `OwnerUserName`.

---
+++ GrooveNest.Domain/DTOs/PlaylistDTOs/PlaylistResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace GrooveNest.Domain.DTOs.PlaylistDTOs
+{
+    public class PlaylistResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public bool IsPublic { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Guid OwnerId { get; set; }
+        public string OwnerUserName { get; set; } = "Unknown";
+    }
+}
