755aa99f80509e4af41412db5ec58073cd3e2157
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 20:50:48 +0100

    Add IAlbumService interface for album management
    Introduces the IAlbumService interface in the GrooveNest.Service.Interfaces namespace. This interface includes asynchronous methods for retrieving, creating, updating, and deleting albums, returning ApiResponse objects with AlbumResponseDto or string results.

---
+++ GrooveNest.Service/Interfaces/IAlbumService.cs
@@ -0,0 +1,15 @@
+﻿using GrooveNest.Domain.DTOs.AlbumDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface IAlbumService
+    {
+        Task<ApiResponse<AlbumResponseDto>> GetAllAlbumAsync();
+        Task<ApiResponse<object>> GetAllPaginatedAlbumAsync(int page = 1, int pageSize = 10, string searchQuery = "");
+        Task<ApiResponse<AlbumResponseDto>> GetAlbumByIdAsync(Guid id);
+        Task<ApiResponse<AlbumResponseDto>> CreateAlbumAsync(AlbumCreateDto albumCreateDto);
+        Task<ApiResponse<AlbumResponseDto>> UpdateAlbumAsync(Guid id, AlbumUpdateDto albumUpdateDto);
+        Task<ApiResponse<string>> DeleteAlbumAsync(Guid id);
+    }
+}
