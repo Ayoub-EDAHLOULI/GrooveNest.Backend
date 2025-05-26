f7532853d69c9f974d7baea6cf537c7e7f28cba2
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 20:44:54 +0100

    Add AlbumResponseDto class for album data transfer
    Introduced a new `AlbumResponseDto` class in the
    `GrooveNest.Domain.DTOs.AlbumDTOs` namespace. This class
    includes properties for `Id`, `Title`, `ReleaseDate`,
    `CoverUrl`, `ArtistId`, and `ArtistName`, with proper
    data types and nullability annotations.

---
+++ GrooveNest.Domain/DTOs/AlbumDTOs/AlbumResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GrooveNest.Domain.DTOs.AlbumDTOs
+{
+    public class AlbumResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime ReleaseDate { get; set; }
+        public string? CoverUrl { get; set; }
+        public Guid ArtistId { get; set; }
+        public string ArtistName { get; set; } = null!;
+    }
+}
