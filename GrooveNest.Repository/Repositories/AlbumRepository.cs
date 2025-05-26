fa65dc8955e5aa37b11e8db472c6d692c399a06f
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 20:48:25 +0100

    Add AlbumRepository with GetAlbumByTitle method
    Introduces the AlbumRepository class that extends
    GenericRepository and implements IAlbumRepository.
    Includes a method to asynchronously retrieve an Album
    by its title, with related Artist data using EF Core.

---
+++ GrooveNest.Repository/Repositories/AlbumRepository.cs
@@ -0,0 +1,17 @@
+﻿using GrooveNest.Domain.Entities;
+using GrooveNest.Repository.Data;
+using GrooveNest.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrooveNest.Repository.Repositories
+{
+    public class AlbumRepository(AppDbContext context) : GenericRepository<Album, Guid>(context), IAlbumRepository
+    {
+        public async Task<Album?> GetAlbumByTitle(string title)
+        {
+            return await _context.Albums
+                .Include(a => a.Artist)
+                .FirstOrDefaultAsync(a => a.Title == title);
+        }
+    }
+}
