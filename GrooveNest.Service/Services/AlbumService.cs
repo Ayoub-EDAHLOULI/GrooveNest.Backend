229ff6fd740f5b8efbd2ab6d10a6106e18441a91
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 20:51:43 +0100

    Add AlbumService class with method stubs
    Introduces the `AlbumService` class implementing the `IAlbumService` interface.
    Includes method stubs for creating, deleting, retrieving, and updating albums,
    all currently throwing `NotImplementedException`. A private readonly field for
    `IAlbumRepository` is also added for future data access operations.

---
+++ GrooveNest.Service/Services/AlbumService.cs
@@ -0,0 +1,42 @@
+﻿using GrooveNest.Domain.DTOs.AlbumDTOs;
+using GrooveNest.Repository.Interfaces;
+using GrooveNest.Service.Interfaces;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Services
+{
+    public class AlbumService(IAlbumRepository albumRepository) : IAlbumService
+    {
+        private readonly IAlbumRepository _albumRepository = albumRepository;
+
+        public Task<ApiResponse<AlbumResponseDto>> CreateAlbumAsync(AlbumCreateDto albumCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> DeleteAlbumAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<AlbumResponseDto>> GetAlbumByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<AlbumResponseDto>> GetAllAlbumAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<object>> GetAllPaginatedAlbumAsync(int page = 1, int pageSize = 10, string searchQuery = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<AlbumResponseDto>> UpdateAlbumAsync(Guid id, AlbumUpdateDto albumUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
