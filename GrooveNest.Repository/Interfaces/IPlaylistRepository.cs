4bae0fbf2e28b9795310e4e2d74b219e23805e63
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 22:41:31 +0100

    Add IPlaylistRepository interface for playlist management
    Introduces the `IPlaylistRepository` interface in the
    `GrooveNest.Repository.Interfaces` namespace. This interface
    extends `IGenericRepository` with `Playlist` and `Guid`
    type parameters and includes a method `GetPlaylistByName`
    to retrieve a nullable `Playlist` by its name.

---
+++ GrooveNest.Repository/Interfaces/IPlaylistRepository.cs
@@ -0,0 +1,9 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface IPlaylistRepository : IGenericRepository<Playlist, Guid>
+    {
+        Task<Playlist?> GetPlaylistByName(string name);
+    }
+}
