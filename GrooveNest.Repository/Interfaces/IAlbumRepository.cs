0814d9e7fb9106546c6cff45640a9c7aaf4da571
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-26 20:46:53 +0100

    Add IAlbumRepository interface for album retrieval
    Introduces the `IAlbumRepository` interface in the
    `GrooveNest.Repository.Interfaces` namespace. This
    interface extends `IGenericRepository` with `Album`
    as the entity type and `Guid` as the key type.
    It includes a method `GetAlbumByTitle` for
    asynchronously retrieving an album by its title.

---
+++ GrooveNest.Repository/Interfaces/IAlbumRepository.cs
@@ -0,0 +1,9 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface IAlbumRepository : IGenericRepository<Album, Guid>
+    {
+        Task<Album?> GetAlbumByTitle(string title);
+    }
+}
