9dca8aa06bf4f37e9b0123fc0db51812914b01db
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-27 22:29:55 +0100

    Add ITrackRepository interface for track retrieval
    Introduces the `ITrackRepository` interface in the
    `GrooveNest.Repository.Interfaces` namespace. This
    interface extends `IGenericRepository` for the `Track`
    entity and includes asynchronous methods to retrieve
    tracks by title, artist ID, album ID, artist name,
    and album title.

---
+++ GrooveNest.Repository/Interfaces/ITrackRepository.cs
@@ -0,0 +1,13 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface ITrackRepository : IGenericRepository<Track, Guid>
+    {
+        Task<Track> GetTrackByTitleAsync(string title);
+        Task<IEnumerable<Track>> GetTracksByArtistIdAsync(Guid artistId);
+        Task<IEnumerable<Track>> GetTracksByAlbumIdAsync(Guid albumId);
+        Task<IEnumerable<Track>> GetTracksByArtistNameAsync(string artistName);
+        Task<IEnumerable<Track>> GetTracksByAlbumTitleAsync(string albumTitle);
+    }
+}
