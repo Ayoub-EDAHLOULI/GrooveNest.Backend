b5fd6cfaee0ab077de94e6ffbcc402db1f00475e
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-27 22:25:10 +0100

    Add TrackCreateDto class for track creation
    Introduces the `TrackCreateDto` class in the
    `GrooveNest.Domain.DTOs.TrackDTOs` namespace. This
    class includes properties for `Title`, `DurationSec`,
    `AudioUrl`, `TrackNumber`, and `ArtistId`, with
    appropriate data types and nullability settings for
    string properties.

---
+++ GrooveNest.Domain/DTOs/TrackDTOs/TrackCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GrooveNest.Domain.DTOs.TrackDTOs
+{
+    public class TrackCreateDto
+    {
+        public string Title { get; set; } = null!;
+        public int DurationSec { get; set; }
+        public string AudioUrl { get; set; } = null!;
+        public int TrackNumber { get; set; }
+        public Guid ArtistId { get; set; }
+    }
+}
