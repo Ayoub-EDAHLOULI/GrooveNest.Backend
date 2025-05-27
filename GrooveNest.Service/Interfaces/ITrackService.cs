591e53519e0213c5a3414617754344e0a6b4c9fe
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-27 22:41:30 +0100

    Add ITrackService interface for track management
    Introduces the `ITrackService` interface in the `GrooveNest.Service.Interfaces` namespace. This interface includes asynchronous methods for retrieving, creating, updating, and deleting tracks, with structured response types such as `ApiResponse<TrackResponseDto>` and `IEnumerable<TrackResponseDto>`.

---
+++ GrooveNest.Service/Interfaces/ITrackService.cs
@@ -0,0 +1,18 @@
+﻿using GrooveNest.Domain.DTOs.TrackDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface ITrackService
+    {
+        Task<ApiResponse<TrackResponseDto>?> GetTrackByIdAsync(Guid id);
+        Task<ApiResponse<TrackResponseDto>?> GetTrackByTitleAsync(string title);
+        Task<IEnumerable<ApiResponse<TrackResponseDto>>> GetTracksByArtistIdAsync(Guid artistId);
+        Task<IEnumerable<ApiResponse<TrackResponseDto>>> GetTracksByAlbumIdAsync(Guid albumId);
+        Task<IEnumerable<ApiResponse<TrackResponseDto>>> GetTracksByArtistNameAsync(string artistName);
+        Task<IEnumerable<ApiResponse<TrackResponseDto>>> GetTracksByAlbumTitleAsync(string albumTitle);
+        Task<ApiResponse<TrackResponseDto>> CreateTrackAsync(TrackCreateDto trackCreateDto);
+        Task<ApiResponse<TrackResponseDto>> UpdateTrackAsync(Guid id, TrackUpdateDto trackUpdateDto);
+        Task<string> DeleteTrackAsync(Guid id);
+    }
+}
