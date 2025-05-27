2a65138bf11f6fd73d1263b29d1ecea81b8d766e
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-27 22:27:31 +0100

    Add TrackResponseDto class for track information
    Introduces the `TrackResponseDto` class in the
    `GrooveNest.Domain.DTOs.TrackDTOs` namespace. This class
    contains properties such as `Id`, `Title`, `DurationSec`,
    `AudioUrl`, `TrackNumber`, `ArtistId`, `ArtistName`,
    `AlbumId`, and `AlbumTitle` to represent track details.
    Key properties are set as non-nullable, while album
    information is optional.

---
+++ GrooveNest.Domain/DTOs/TrackDTOs/TrackResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace GrooveNest.Domain.DTOs.TrackDTOs
+{
+    public class TrackResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int DurationSec { get; set; }
+        public string AudioUrl { get; set; } = null!;
+        public int TrackNumber { get; set; }
+
+        public Guid ArtistId { get; set; }
+        public string ArtistName { get; set; } = null!;
+
+        public Guid? AlbumId { get; set; }
+        public string? AlbumTitle { get; set; }
+    }
+}
