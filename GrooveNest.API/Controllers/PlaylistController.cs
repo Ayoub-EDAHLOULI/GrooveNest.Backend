b233285f530b49a9bd78d0ec4990d673bf1e38ce
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-27 22:05:47 +0100

    Add GetAllPlaylistAsync method to PlaylistController
    Implemented an asynchronous HTTP GET endpoint in the PlaylistController to retrieve all playlists using the PlaylistService. The method returns a NotFound response if the retrieval fails, or an Ok response with the playlists if successful. Added comments to clarify the section related to GetAllAlbumAsync methods.

--- GrooveNest.API/Controllers/PlaylistController.cs
+++ GrooveNest.API/Controllers/PlaylistController.cs
@@ -8,5 +8,21 @@ namespace GrooveNest.API.Controllers
     public class PlaylistController(PlaylistService playlistService) : ControllerBase
     {
         private readonly PlaylistService _playlistService = playlistService;
+
+
+        // ------------------------------------------------------------------------- //
+        // ------------------------ GetAllAlbumAsync METHODS ----------------------- //
+        // ------------------------------------------------------------------------- //
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllPlaylistAsync()
+        {
+            var response = await _playlistService.GetAllPlaylistsAsync();
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
     }
 }
