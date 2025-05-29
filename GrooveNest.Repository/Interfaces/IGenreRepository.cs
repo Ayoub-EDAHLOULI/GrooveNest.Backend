f0c68f67b2d40653fdbbfd21f1095b9176a50018
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-29 22:24:35 +0100

    Add IGenreRepository interface for genre management
    Introduces the `IGenreRepository` interface in the
    `GrooveNest.Repository.Interfaces` namespace. This
    interface extends `IGenericRepository` for the `Genre`
    entity and includes a method `GetGenreByName` to
    retrieve genres by their name asynchronously.

---
+++ GrooveNest.Repository/Interfaces/IGenreRepository.cs
@@ -0,0 +1,9 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface IGenreRepository : IGenericRepository<Genre, int>
+    {
+        Task<Genre> GetGenreByName(string name);
+    }
+}
