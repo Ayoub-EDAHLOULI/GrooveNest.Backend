f57e54cfdac8892e39fae9349ca6b41c3821a925
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-29 22:33:58 +0100

    Add GenreService class implementing IGenreService
    Introduces the GenreService class with methods for managing genres, including create, delete, retrieve, and update functionalities. All methods currently throw NotImplementedException. The class utilizes IGenreRepository and includes necessary using directives.

---
+++ GrooveNest.Service/Services/GenreService.cs
@@ -0,0 +1,42 @@
+﻿using GrooveNest.Domain.DTOs.GenreDTOs;
+using GrooveNest.Repository.Interfaces;
+using GrooveNest.Service.Interfaces;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Services
+{
+    public class GenreService(IGenreRepository genreRepository) : IGenreService
+    {
+        private readonly IGenreRepository _genreRepository = genreRepository;
+
+        public Task<ApiResponse<GenreResponseDto>> CreateGenreAsync(GenreCreateDto genreCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> DeleteGenreAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<List<GenreResponseDto>>> GetAllGenresAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<object>> GetAllPaginatedGenresAsync(int page = 1, int pageSize = 10, string searchQuery = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<GenreResponseDto>> GetGenreByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<GenreResponseDto>> UpdateGenreAsync(Guid id, GenreUpdateDto genreUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
