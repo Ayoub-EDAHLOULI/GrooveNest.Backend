f55c8c079fab975dc670d06a19b106d70072b34a
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-29 22:31:45 +0100

    Add IGenreService interface for genre management
    Introduces the IGenreService interface in the GrooveNest.Service.Interfaces namespace.
    Defines asynchronous methods for retrieving, creating, updating, and deleting genres,
    returning ApiResponse types with relevant DTOs. Includes necessary using directives.

---
+++ GrooveNest.Service/Interfaces/IGenreService.cs
@@ -0,0 +1,15 @@
+﻿using GrooveNest.Domain.DTOs.GenreDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface IGenreService
+    {
+        Task<ApiResponse<List<GenreResponseDto>>> GetAllGenresAsync();
+        Task<ApiResponse<object>> GetAllPaginatedGenresAsync(int page = 1, int pageSize = 10, string searchQuery = "");
+        Task<ApiResponse<GenreResponseDto>> GetGenreByIdAsync(Guid id);
+        Task<ApiResponse<GenreResponseDto>> CreateGenreAsync(GenreCreateDto genreCreateDto);
+        Task<ApiResponse<GenreResponseDto>> UpdateGenreAsync(Guid id, GenreUpdateDto genreUpdateDto);
+        Task<ApiResponse<string>> DeleteGenreAsync(Guid id);
+    }
+}
