a17ac13e9ab1cf2107ed077597a636041c81577d
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-29 23:06:51 +0100

    Add GenreController for managing genre data
    Introduces a new `GenreController` in the `GrooveNest.API.Controllers` namespace.
    Includes routing and API controller attributes, along with a constructor for injecting
    `GenreService`. Implements `GetAllGenresAsync` method to retrieve and return all
    genres with appropriate HTTP responses based on the operation's success.

---
+++ GrooveNest.API/Controllers/GenreController.cs
@@ -0,0 +1,28 @@
+﻿using GrooveNest.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooveNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController(GenreService genreService) : ControllerBase
+    {
+        private readonly GenreService _genreService = genreService;
+
+
+        // -------------------------------------------------------------------------- //
+        // ------------------------ GetAllGenresAsync METHODS ----------------------- //
+        // -------------------------------------------------------------------------- // 
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllGenresAsync()
+        {
+            var response = await _genreService.GetAllGenresAsync();
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+    }
+}
