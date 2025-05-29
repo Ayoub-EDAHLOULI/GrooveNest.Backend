c08b223612fbd338390194f964b9d7393f6b7a63
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-29 22:38:02 +0100

    Add pagination and search to genre retrieval
    - Moved `CreateGenreAsync` method above `DeleteGenreAsync`.
    - Implemented `GetAllPaginatedGenresAsync` for retrieving genres with pagination and optional search.
    - Updated method to handle genre existence checks and return a paginated response.
    - `DeleteGenreAsync` remains unchanged in its implementation.

--- GrooveNest.Service/Services/GenreService.cs
+++ GrooveNest.Service/Services/GenreService.cs
@@ -30,17 +30,49 @@ namespace GrooveNest.Service.Services
             return ApiResponse<List<GenreResponseDto>>.SuccessResponse(genreDtos, "Genres retrieved successfully.");
         }
 
+
+        // -------------------------------------------------------------------------- //
+        // ------------------------ GetAllGenresAsync METHODS ----------------------- //
+        // -------------------------------------------------------------------------- // 
+        public async Task<ApiResponse<object>> GetAllPaginatedGenresAsync(int page = 1, int pageSize = 10, string searchQuery = "")
+        {
+            var genres = await _genreRepository.GetAllAsync();
+            if (genres == null || !genres.Any())
+            {
+                return ApiResponse<object>.ErrorResponse("No genres found.");
+            }
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                genres = genres.Where(g => g.Name.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            var totalCount = genres.Count();
+            var paginatedGenres = genres
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(g => new GenreResponseDto
+                {
+                    Id = g.Id,
+                    Name = g.Name,
+                })
+                .ToList();
+
+            var response = new
+            {
+                PaginatedGenres = paginatedGenres,
+                GenreCount = totalCount
+            };
+
+            return ApiResponse<object>.SuccessResponse(response, "Genres retrieved successfully.");
+        }
+
         public Task<ApiResponse<GenreResponseDto>> CreateGenreAsync(GenreCreateDto genreCreateDto)
         {
             throw new NotImplementedException();
         }
 
         public Task<ApiResponse<string>> DeleteGenreAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ApiResponse<object>> GetAllPaginatedGenresAsync(int page = 1, int pageSize = 10, string searchQuery = "")
         {
             throw new NotImplementedException();
         }
