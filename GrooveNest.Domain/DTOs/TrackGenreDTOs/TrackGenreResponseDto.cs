bd0ac4cfa2fe78127f1a051e6a02290235116eeb
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-31 09:55:20 +0100

    Add TrackGenreResponseDto class for track details
    Introduces the `TrackGenreResponseDto` class in the
    `GrooveNest.Domain.DTOs.TrackGenreDTOs` namespace. This
    class includes properties for `TrackId`, `GenreId`,
    `TrackName`, `GenreName`, `ArtistName`, and `AlbumName`
    to encapsulate information about tracks and their genres.

---
+++ GrooveNest.Domain/DTOs/TrackGenreDTOs/TrackGenreResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GrooveNest.Domain.DTOs.TrackGenreDTOs
+{
+    public class TrackGenreResponseDto
+    {
+        public Guid TrackId { get; set; }
+        public int GenreId { get; set; }
+        public string? TrackName { get; set; }
+        public string? GenreName { get; set; }
+        public string? ArtistName { get; set; }
+        public string? AlbumName { get; set; }
+    }
+}
