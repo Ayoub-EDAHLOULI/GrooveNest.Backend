211b99eceb3221342c0225a7a29aebd002322c13
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-31 09:57:25 +0100

    Add TrackUpdateDto for updating track genre information
    Introduces a new class `TrackUpdateDto` in the namespace
    `GrooveNest.Domain.DTOs.TrackGenreDTOs`. This class includes
    two properties: `TrackId` (nullable Guid) and `GenreId`
    (nullable int) to facilitate the updating of track genre
    information in the application.

---
+++ GrooveNest.Domain/DTOs/TrackGenreDTOs/TrackUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GrooveNest.Domain.DTOs.TrackGenreDTOs
+{
+    public class TrackUpdateDto
+    {
+        public Guid? TrackId { get; set; }
+        public int? GenreId { get; set; }
+    }
+}
