c00d2a42caa1ce2ab5350cb852c93311a911ac63
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-31 09:56:00 +0100

    Add TrackGenreCreateDto class for track-genre mapping
    Introduces a new class `TrackGenreCreateDto` in the
    `GrooveNest.Domain.DTOs.TrackGenreDTOs` namespace.
    This class includes properties `TrackId` (Guid) and
    `GenreId` (int) to represent the relationship between
    tracks and genres.

---
+++ GrooveNest.Domain/DTOs/TrackGenreDTOs/TrackGenreCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GrooveNest.Domain.DTOs.TrackGenreDTOs
+{
+    public class TrackGenreCreateDto
+    {
+        public Guid TrackId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
