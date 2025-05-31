8af78dd7f3c5b6a65aedd11b1c6d088c2ac4be49
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-31 10:07:59 +0100

    Add ITrackGenreService interface for track genre management
    Introduces the `ITrackGenreService` interface in the
    `GrooveNest.Service.Interfaces` namespace. This interface
    provides asynchronous methods for managing track genres,
    including retrieval, addition, deletion, and fetching
    related genres and tracks. The methods return
    `ApiResponse` objects with relevant DTOs, enhancing
    functionality for track-genre relationships.

---
+++ GrooveNest.Service/Interfaces/ITrackGenreService.cs
@@ -0,0 +1,17 @@
+﻿using GrooveNest.Domain.DTOs.GenreDTOs;
+using GrooveNest.Domain.DTOs.TrackDTOs;
+using GrooveNest.Domain.DTOs.TrackGenreDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface ITrackGenreService
+    {
+        Task<ApiResponse<List<TrackGenreResponseDto>>> GetAllTrackGenresAsync();
+        Task<ApiResponse<TrackGenreResponseDto>> GetTrackGenreByIdAsync(Guid trackId);
+        Task<ApiResponse<TrackGenreResponseDto>> AddTrackGenreAsync(TrackGenreCreateDto trackGenreCreateDto);
+        Task<ApiResponse<string>> DeleteTrackGenreAsync(Guid trackId, Guid genreId);
+        Task<ApiResponse<List<GenreResponseDto>>> GetGenresByTrackIdAsync(Guid trackId);
+        Task<ApiResponse<List<TrackResponseDto>>> GetTracksByGenreIdAsync(Guid genreId);
+    }
+}
