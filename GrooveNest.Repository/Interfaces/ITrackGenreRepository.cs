dc97cab923c9a11dae2af89d21a438609e375165
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-31 10:05:39 +0100

    Update ITrackGenreRepository and add TrackGenreRepository
    Changed genreId type to Guid in ITrackGenreRepository and removed GetByIdsAsync method. Added TrackGenreRepository class implementing the interface with methods for managing track genres using Entity Framework Core.

--- GrooveNest.Repository/Interfaces/ITrackGenreRepository.cs
+++ GrooveNest.Repository/Interfaces/ITrackGenreRepository.cs
@@ -5,11 +5,10 @@ namespace GrooveNest.Repository.Interfaces
     public interface ITrackGenreRepository
     {
         Task<IEnumerable<TrackGenre>> GetAllAsync();
-        Task<TrackGenre?> GetByIdsAsync(Guid trackId, int genreId);
         Task<TrackGenre?> GetByIdAsync(Guid trackId);
         Task AddAsync(TrackGenre entity);
         Task DeleteAsync(TrackGenre entity);
         Task<List<Genre>> GetGenresByTrackIdAsync(Guid trackId);
-        Task<List<Track>> GetTracksByGenreIdAsync(int genreId);
+        Task<List<Track>> GetTracksByGenreIdAsync(Guid genreId);
     }
 }
---
+++ GrooveNest.Repository/Repositories/TrackGenreRepository.cs
@@ -0,0 +1,54 @@
+﻿using GrooveNest.Domain.Entities;
+using GrooveNest.Repository.Data;
+using GrooveNest.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrooveNest.Repository.Repositories
+{
+    public class TrackGenreRepository(AppDbContext context) : ITrackGenreRepository
+    {
+        public async Task AddAsync(TrackGenre entity)
+        {
+            await context.TrackGenres.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TrackGenre entity)
+        {
+            context.TrackGenres.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TrackGenre>> GetAllAsync()
+        {
+            return await context.TrackGenres
+                .Include(tg => tg.Track)
+                .Include(tg => tg.Genre)
+                .ToListAsync();
+        }
+
+        public async Task<TrackGenre?> GetByIdAsync(Guid trackId)
+        {
+            return await context.TrackGenres
+                .Include(tg => tg.Track)
+                .Include(tg => tg.Genre)
+                .FirstOrDefaultAsync(tg => tg.TrackId == trackId);
+        }
+
+        public async Task<List<Genre>> GetGenresByTrackIdAsync(Guid trackId)
+        {
+            return await context.TrackGenres
+                .Where(tg => tg.TrackId == trackId)
+                .Select(tg => tg.Genre)
+                .ToListAsync();
+        }
+
+        public async Task<List<Track>> GetTracksByGenreIdAsync(Guid genreId)
+        {
+            return await context.TrackGenres
+                .Where(tg => tg.GenreId == genreId)
+                .Select(tg => tg.Track)
+                .ToListAsync();
+        }
+    }
+}
