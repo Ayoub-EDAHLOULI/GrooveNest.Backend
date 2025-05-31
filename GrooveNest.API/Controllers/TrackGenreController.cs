450f0434f4fee53f0300f41c2a0e17c361a620cd
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-05-31 12:22:50 +0100

    Add TrackGenreController for managing track genres
    Introduces the `TrackGenreController` class in the
    `GrooveNest.API.Controllers` namespace. This API controller
    handles HTTP requests for creating track genres. It uses
    dependency injection for `TrackGenreService` and includes
    the `CreateTrackGenreAsync` method to process POST requests
    with `TrackGenreCreateDto`, returning appropriate responses
    based on the operation's success.

---
+++ GrooveNest.API/Controllers/TrackGenreController.cs
@@ -0,0 +1,30 @@
+﻿using GrooveNest.Domain.DTOs.TrackGenreDTOs;
+using GrooveNest.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooveNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackGenreController(TrackGenreService trackGenreService) : ControllerBase
+    {
+        private readonly TrackGenreService _trackGenreService = trackGenreService;
+
+
+        // ------------------------------------------------------------------------------ //
+        // ------------------------ CreateTrackGenreAsync METHODS ----------------------- //
+        // ------------------------------------------------------------------------------ //
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTrackGenreAsync([FromBody] TrackGenreCreateDto trackGenreCreateDto)
+        {
+            var response = await _trackGenreService.CreateTrackGenreAsync(trackGenreCreateDto);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response.Data);
+        }
+
+    }
+}
