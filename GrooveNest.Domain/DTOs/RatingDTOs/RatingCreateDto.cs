8d2479ee611c1f1d04f4016c4ccf650690002bb7
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 23:36:37 +0100

    Add RatingCreateDto class for track ratings
    Introduces the `RatingCreateDto` class in the
    `GrooveNest.Domain.DTOs.RatingDTOs` namespace.
    This class includes properties for `Stars`,
    `TrackId`, and `UserId`, facilitating the
    representation of user ratings for tracks.

---
+++ GrooveNest.Domain/DTOs/RatingDTOs/RatingCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GrooveNest.Domain.DTOs.RatingDTOs
+{
+    public class RatingCreateDto
+    {
+        public int Stars { get; set; }
+        public Guid TrackId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
