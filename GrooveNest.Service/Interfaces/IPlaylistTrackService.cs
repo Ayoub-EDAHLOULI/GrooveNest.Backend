6307a0a4410d109f3ed52409a7cdfe75b438622a
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 21:41:56 +0100

    Add IPlaylistTrackService interface for track management
    This commit introduces the `IPlaylistTrackService` interface in the `GrooveNest.Service.Interfaces` namespace. It defines four asynchronous methods for managing playlist tracks: retrieving tracks by playlist ID, getting a specific track by playlist and track IDs, adding a track to a playlist, and removing a track from a playlist. The methods are designed for asynchronous operations, utilizing `Task` and `List<PlaylistTrack>` as return types.

---
+++ GrooveNest.Service/Interfaces/IPlaylistTrackService.cs
@@ -0,0 +1,12 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface IPlaylistTrackService
+    {
+        Task<List<PlaylistTrack>> GetTracksByPlaylistIdAsync(Guid playlistId);
+        Task<PlaylistTrack?> GetByPlaylistAndTrackAsync(Guid playlistId, Guid trackId);
+        Task<PlaylistTrack> AddTrackToPlaylistAsync(Guid playlistId, Guid trackId);
+        Task<PlaylistTrack> RemoveTrackFromPlaylistAsync(Guid playlistId, Guid trackId);
+    }
+}
