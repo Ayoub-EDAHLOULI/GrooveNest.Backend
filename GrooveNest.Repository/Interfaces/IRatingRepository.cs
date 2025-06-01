da39b8d9fb775d11a2249e7bc99f43ad6224d447
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 23:41:44 +0100

    Add IRatingRepository interface for rating management
    Introduces the `IRatingRepository` interface in the
    `GrooveNest.Repository.Interfaces` namespace. This interface
    extends a generic repository for the `Rating` entity and
    includes three asynchronous methods for retrieving ratings
    by track and user IDs, fetching ratings for a specific
    track, and calculating the average rating of a track.

---
+++ GrooveNest.Repository/Interfaces/IRatingRepository.cs
@@ -0,0 +1,11 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface IRatingRepository : IGenericRepository<Rating, int>
+    {
+        Task<Rating?> GetByTrackIdAndUserIdAsync(Guid trackId, Guid userId);
+        Task<List<Rating>> GetRatingsByTrackIdAsync(Guid trackId);
+        Task<double> GetAverageRatingByTrackIdAsync(Guid trackId);
+    }
+}
