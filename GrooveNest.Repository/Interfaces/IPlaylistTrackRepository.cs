bbcafa73a9cfb20eb165145be7bfee762a29e4ee
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 21:31:31 +0100

    Add IPlaylistTrackRepository interface
    Introduces the IPlaylistTrackRepository interface in the
    GrooveNest.Repository.Interfaces namespace. This interface
    extends IGenericRepository for PlaylistTrack and includes
    two asynchronous methods: GetTracksByPlaylistIdAsync
    and GetByPlaylistAndTrackAsync for retrieving tracks
    by playlist ID and specific track details, respectively.

---
+++ GrooveNest.Repository/Interfaces/IPlaylistTrackRepository.cs
@@ -0,0 +1,10 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface IPlaylistTrackRepository : IGenericRepository<PlaylistTrack, Guid>
+    {
+        Task<List<PlaylistTrack>> GetTracksByPlaylistIdAsync(Guid playlistId);
+        Task<PlaylistTrack?> GetByPlaylistAndTrackAsync(Guid playlistId, Guid trackId);
+    }
+}
