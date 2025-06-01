676cec9a4c6d29c152bf0a6272aed36d08ed053e
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 21:34:23 +0100

    Add PlaylistTrackRepository for database interactions
    Introduces `PlaylistTrackRepository` class inheriting from
    `GenericRepository<PlaylistTrack, Guid>` and implementing
    `IPlaylistTrackRepository`. Adds methods to retrieve
    `PlaylistTrack` entities by playlist and track IDs, as well
    as to fetch tracks associated with a specific playlist ID,
    including related `Track` and `Playlist` entities.

---
+++ GrooveNest.Repository/Repositories/PlaylistTrackRepository.cs
@@ -0,0 +1,25 @@
+﻿using GrooveNest.Domain.Entities;
+using GrooveNest.Repository.Data;
+using GrooveNest.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrooveNest.Repository.Repositories
+{
+    public class PlaylistTrackRepository(AppDbContext context) : GenericRepository<PlaylistTrack, Guid>(context), IPlaylistTrackRepository
+    {
+        public async Task<PlaylistTrack?> GetByPlaylistAndTrackAsync(Guid playlistId, Guid trackId)
+        {
+            return await _context.PlaylistTracks
+                .FirstOrDefaultAsync(pt => pt.PlaylistId == playlistId && pt.TrackId == trackId);
+        }
+
+        public async Task<List<PlaylistTrack>> GetTracksByPlaylistIdAsync(Guid playlistId)
+        {
+            return await _context.PlaylistTracks
+                .Include(pt => pt.Track) // Include related Track entity
+                .Include(pt => pt.Playlist) // Include related Playlist entity
+                .Where(pt => pt.PlaylistId == playlistId)
+                .ToListAsync();
+        }
+    }
+}
