54201e5d4c2d3fd1200f634244a0362d410031cb
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 14:23:17 +0100

    Create the ILikeService with three methods GetTrackLikesAsync, CreateLikeAsync and DeleteLikeAsync

---
+++ GrooveNest.Service/Interfaces/ILikeService.cs
@@ -0,0 +1,11 @@
+﻿using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface ILikeService
+    {
+        Task<ApiResponse<object>> GetTrackLikesAsync(Guid trackId);
+        Task<ApiResponse<string>> CreateLikeAsync(Guid trackId, Guid UserId);
+        Task<ApiResponse<string>> DeleteLikeAsync(Guid trackId, Guid UserId);
+    }
+}
