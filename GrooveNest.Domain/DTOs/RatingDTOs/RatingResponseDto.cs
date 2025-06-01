8c13cc374d42f0575aa688b3c8fd13558527e64c
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 23:37:37 +0100

    Add RatingResponseDto class for rating functionality
    Introduces the `RatingResponseDto` class in the
    `GrooveNest.Domain.DTOs.RatingDTOs` namespace. This class
    contains properties for `Id`, `Stars`, `CreatedAt`,
    `TrackId`, `TrackTitle`, `UserId`, and `UserName`,
    with default values for `TrackTitle` and `UserName`.

---
+++ GrooveNest.Domain/DTOs/RatingDTOs/RatingResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace GrooveNest.Domain.DTOs.RatingDTOs
+{
+    public class RatingResponseDto
+    {
+        public int Id { get; set; }
+        public int Stars { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Guid TrackId { get; set; }
+        public string TrackTitle { get; set; } = string.Empty;
+
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+    }
+}
