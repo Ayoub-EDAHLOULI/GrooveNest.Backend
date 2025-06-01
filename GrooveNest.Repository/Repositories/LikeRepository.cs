4542009594e78942a3f2265f60aee6ad717049f4
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 14:16:37 +0100

    Create the Like repository

---
+++ GrooveNest.Repository/Repositories/LikeRepository.cs
@@ -0,0 +1,10 @@
+﻿using GrooveNest.Domain.Entities;
+using GrooveNest.Repository.Data;
+using GrooveNest.Repository.Interfaces;
+
+namespace GrooveNest.Repository.Repositories
+{
+    public class LikeRepository(AppDbContext context) : GenericRepository<Like, Guid>(context), ILikeRepository
+    {
+    }
+}
