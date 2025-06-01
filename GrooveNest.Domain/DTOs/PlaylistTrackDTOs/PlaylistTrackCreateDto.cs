c702a7d45297aebb4ea2a87b5f831a3284c5ad95
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 21:22:52 +0100

    Add PlaylistTrackCreateDto class for playlist tracks
    Introduces the `PlaylistTrackCreateDto` class in the
    `GrooveNest.Domain.DTOs.PlaylistTrackDTOs` namespace.
    This class includes properties for `PlaylistId`, `TrackId`,
    and `TrackNumber`, facilitating the representation of
    tracks being added to a playlist.

---
+++ GrooveNest.Domain/DTOs/PlaylistTrackDTOs/PlaylistTrackCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GrooveNest.Domain.DTOs.PlaylistTrackDTOs
+{
+    public class PlaylistTrackCreateDto
+    {
+        public Guid PlaylistId { get; set; }
+        public Guid TrackId { get; set; }
+        public int TrackNumber { get; set; }
+    }
+}
