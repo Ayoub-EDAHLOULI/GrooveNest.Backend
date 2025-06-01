82009a82e67ea49452bffdfd47451d73da994870
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 21:23:27 +0100

    Add PlaylistTrackResponseDto class for track details
    Introduces the `PlaylistTrackResponseDto` class in the
    `GrooveNest.Domain.DTOs.PlaylistTrackDTOs` namespace.
    This class contains properties for `PlaylistId`,
    `PlaylistTitle`, `TrackId`, `TrackTitle`, and `TrackNumber`
    to represent details about a playlist track response.

---
+++ GrooveNest.Domain/DTOs/PlaylistTrackDTOs/PlaylistTrackResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace GrooveNest.Domain.DTOs.PlaylistTrackDTOs
+{
+    public class PlaylistTrackResponseDto
+    {
+        public Guid PlaylistId { get; set; }
+        public string PlaylistTitle { get; set; } = string.Empty;
+
+        public Guid TrackId { get; set; }
+        public string TrackTitle { get; set; } = string.Empty;
+
+        public int TrackNumber { get; set; }
+    }
+}
