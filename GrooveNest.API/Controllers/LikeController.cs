630812a3ef7b6e8b7305240bdaac8db1281e875d
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 21:03:26 +0100

    Add LikeController for managing track likes
    Introduces a new `LikeController` in the `GrooveNest.API.Controllers` namespace. This controller handles HTTP requests for creating likes on tracks using a `LikeService`. The `CreateLikeAsync` method processes POST requests with `trackId` and `userId`, returning appropriate responses based on the operation's success.

---
+++ GrooveNest.API/Controllers/LikeController.cs
@@ -0,0 +1,28 @@
+﻿using GrooveNest.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooveNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikeController(LikeService likeService) : ControllerBase
+    {
+        private readonly LikeService _likeService = likeService;
+
+
+        // ------------------------------------------------------------------------ //
+        // ------------------------ CreateLikeAsync METHODS ----------------------- //
+        // ------------------------------------------------------------------------ // 
+
+        [HttpPost("{trackId}/{userId}")]
+        public async Task<IActionResult> CreateLikeAsync(Guid trackId, Guid userId)
+        {
+            var response = await _likeService.CreateLikeAsync(trackId, userId);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
