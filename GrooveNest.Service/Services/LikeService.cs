07f2f96a35dcab421573ba9da4bd95e80c30b420
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-01 14:27:34 +0100

    Create LikeService and initialize the repositories and implement the ILikeSerivce

---
+++ GrooveNest.Service/Services/LikeService.cs
@@ -0,0 +1,28 @@
+﻿using GrooveNest.Repository.Interfaces;
+using GrooveNest.Service.Interfaces;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Services
+{
+    public class LikeService(ILikeRepository likeRepository, IUserRepository userRepository, ITrackRepository trackRepository) : ILikeService
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly ITrackRepository _trackRepository = trackRepository;
+        private readonly ILikeRepository _likeRepository = likeRepository;
+
+        public Task<ApiResponse<string>> CreateLikeAsync(Guid trackId, Guid UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> DeleteLikeAsync(Guid trackId, Guid UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<object>> GetTrackLikesAsync(Guid trackId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
