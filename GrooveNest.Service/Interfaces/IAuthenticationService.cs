b9aaf9b9001547e6566e99a32441dea4ef2ab1db
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 23:14:46 +0100

    Add IAuthenticationService interface for login functionality
    Introduces a new generic interface `IAuthenticationService` in the `GrooveNest.Service.Interfaces` namespace. This interface includes an asynchronous method `LoginAsync` for handling login operations, accepting a login DTO and returning an API response DTO. Also adds a using directive for `GrooveNest.Utilities`.

---
+++ GrooveNest.Service/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface IAuthenticationService<TLoginDto, TResponseDto>
+    {
+        Task<ApiResponse<TResponseDto>> LoginAsync(TLoginDto loginDto);
+    }
+}
