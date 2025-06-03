c622978dd9a74601515759bbbb20b2f99c4b8253
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 23:19:11 +0100

    Add AuthenticationService class for login functionality
    Introduces the `AuthenticationService` class in the
    `GrooveNest.Service.Services` namespace, implementing the
    `IAuthenticationService` interface with `LoginDto` and
    `string`. The class includes a constructor for
    `IUserRepository` and a `LoginAsync` method that
    currently throws a `NotImplementedException`.
    Additional using directives are added for necessary
    namespaces.

---
+++ GrooveNest.Service/Services/AuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using GrooveNest.Domain.DTOs.LoginDTOs;
+using GrooveNest.Repository.Interfaces;
+using GrooveNest.Service.Interfaces;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Services
+{
+    public class AuthenticationService(IUserRepository userRepository) : IAuthenticationService<LoginDto, string>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+
+        public Task<ApiResponse<string>> LoginAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
