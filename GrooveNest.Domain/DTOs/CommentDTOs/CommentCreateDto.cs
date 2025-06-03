d954643e571921e990e15e659cf678140f7f61e0
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 20:22:24 +0100

    Add CommentCreateDto class for comment creation
    Introduces the `CommentCreateDto` class in the
    `GrooveNest.Domain.DTOs.CommentDTOs` namespace.
    This class contains three properties: `Content` (non-nullable string),
    `TrackId` (GUID), and `UserId` (GUID) for handling
    comment creation data.

---
+++ GrooveNest.Domain/DTOs/CommentDTOs/CommentCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GrooveNest.Domain.DTOs.CommentDTOs
+{
+    public class CommentCreateDto
+    {
+        public string Content { get; set; } = null!;
+        public Guid TrackId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
