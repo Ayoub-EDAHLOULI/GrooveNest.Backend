518235d535d8baf0847d6185fe2cd6859612bd5a
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 22:22:28 +0100

    Refactor CommentService for dependency injection
    The `CommentService` class has been refactored to utilize dependency injection by accepting `ICommentRepository`, `IUserRepository`, and `ITrackRepository` through its constructor. Several using directives have been added for necessary namespaces.
    
    A new method, `CreateCommentAsync`, has been implemented to handle comment creation with user and track validation. Additionally, three methods (`DeleteCommentAsync`, `GetCommentsByTrackId`, and `UpdateCommentAsync`) have been declared for future functionality.
    
    These changes enhance the comment management capabilities of the service.

--- GrooveNest.Service/Services/CommentService.cs
+++ GrooveNest.Service/Services/CommentService.cs
@@ -1,12 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GrooveNest.Domain.DTOs.CommentDTOs;
+using GrooveNest.Domain.DTOs.RatingDTOs;
+using GrooveNest.Domain.Entities;
+using GrooveNest.Repository.Interfaces;
+using GrooveNest.Service.Interfaces;
+using GrooveNest.Utilities;
 
 namespace GrooveNest.Service.Services
 {
-    public class CommentService
+    public class CommentService(ICommentRepository commentRepository, IUserRepository userRepository, ITrackRepository trackRepository) : ICommentService
     {
+        private readonly ICommentRepository _commentRepository = commentRepository;
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly ITrackRepository _trackRepository = trackRepository;
+
+
+
+        // --------------------------------------------------------------------------- //
+        // ------------------------ CreateCommentAsync METHODS ----------------------- //
+        // --------------------------------------------------------------------------- // 
+        public async Task<ApiResponse<string>> CreateCommentAsync(CommentCreateDto commentCreateDto)
+        {
+            // Check if the user exists
+            var user = await _userRepository.GetByIdAsync(commentCreateDto.UserId);
+            if (user == null)
+            {
+                return ApiResponse<string>.ErrorResponse("User not found.");
+            }
+
+            // Check if the track exists
+            var track = await _trackRepository.GetByIdAsync(commentCreateDto.TrackId);
+            if (track == null)
+            {
+                return ApiResponse<string>.ErrorResponse("Track not found.");
+            }
+
+            // Check if the content not empty
+            if (!StringValidator.IsNullOrWhiteSpace(commentCreateDto.Content))
+            {
+                return ApiResponse<string>.ErrorResponse("Comment is required");
+            }
+
+            // Create a new comment entity
+            var comment = new Comment
+            {
+                Content = commentCreateDto.Content,
+                UserId = user.Id,
+                TrackId = track.Id,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            // Save the comment
+            await _commentRepository.AddAsync(comment);
+
+            // Return a response 
+            return ApiResponse<string>.SuccessResponse(string.Empty, "Comment created successfully.");
+
+        }
+
+        public Task<ApiResponse<bool>> DeleteCommentAsync(Guid commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<List<CommentResponseDto>>> GetCommentsByTrackId(Guid trackId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> UpdateCommentAsync(CommentUpdateDto commentUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
