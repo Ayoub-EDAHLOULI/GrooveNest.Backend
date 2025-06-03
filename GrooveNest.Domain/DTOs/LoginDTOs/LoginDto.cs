3c870bbc17e42b5f5abc5503e7defd036c9b1a20
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 23:12:00 +0100

    Add LoginDto class for user authentication
    Introduces the `LoginDto` class in the `GrooveNest.Domain.DTOs.LoginDTOs` namespace.
    This class includes two properties, `Email` and `Password`, both of which are required strings initialized to `null!`.

---
+++ GrooveNest.Domain/DTOs/LoginDTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GrooveNest.Domain.DTOs.LoginDTOs
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+    }
+}
