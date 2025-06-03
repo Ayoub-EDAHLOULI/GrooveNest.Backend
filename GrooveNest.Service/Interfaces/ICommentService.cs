c4ff08e5606bafa7878078637cbf603a9d7f37c4
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 22:12:37 +0100

    Add ICommentService interface and CommentService class
    Introduces the ICommentService interface for managing comments, including methods for retrieving, creating, updating, and deleting comments. Also adds an internal CommentService class, which currently has no implementation.

---
+++ GrooveNest.Service/Interfaces/ICommentService.cs
@@ -0,0 +1,13 @@
+﻿using GrooveNest.Domain.DTOs.CommentDTOs;
+using GrooveNest.Utilities;
+
+namespace GrooveNest.Service.Interfaces
+{
+    public interface ICommentService
+    {
+        Task<ApiResponse<List<CommentResponseDto>>> GetCommentsByTrackId(Guid trackId);
+        Task<ApiResponse<string>> CreateCommentAsync(CommentCreateDto commentCreateDto);
+        Task<ApiResponse<string>> UpdateCommentAsync(CommentUpdateDto commentUpdateDto);
+        Task<ApiResponse<bool>> DeleteCommentAsync(Guid commentId);
+    }
+}
