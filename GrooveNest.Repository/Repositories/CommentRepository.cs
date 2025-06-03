514fb4deb7f884c9d1887b7e8fde38cfa2953600
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 22:08:54 +0100

    Add CommentRepository for managing comments
    Introduces a new `CommentRepository` class in the
    `GrooveNest.Repository.Repositories` namespace. This class
    inherits from `GenericRepository<Comment, int>` and implements
    the `ICommentRepository` interface. It provides methods for
    retrieving comments by track and user IDs, counting comments
    by track ID, and fetching comments associated with a specific
    track ID, utilizing Entity Framework Core for asynchronous
    database operations.

---
+++ GrooveNest.Repository/Repositories/CommentRepository.cs
@@ -0,0 +1,31 @@
+﻿using GrooveNest.Domain.Entities;
+using GrooveNest.Repository.Data;
+using GrooveNest.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrooveNest.Repository.Repositories
+{
+    public class CommentRepository(AppDbContext context) : GenericRepository<Comment, int>(context), ICommentRepository
+    {
+        public async Task<Comment?> GetByTrackIdAndUserIdAsync(Guid trackId, Guid userId)
+        {
+            return await _context.Comments
+                .FirstOrDefaultAsync(c => c.TrackId == trackId && c.UserId == userId);
+        }
+
+        public async Task<int> GetCommentCountByTrackIdAsync(Guid trackId)
+        {
+            return await _context.Comments
+                .CountAsync(c => c.TrackId == trackId);
+        }
+
+        public async Task<List<Comment>> GetCommentsByTrackIdAsync(Guid trackId)
+        {
+            return await _context.Comments
+                .Include(c => c.User)
+                .Include(c => c.Track)
+                .Where(c => c.TrackId == trackId)
+                .ToListAsync();
+        }
+    }
+}
