94b2f20924dd798c828f07b0b5267495f88ad5e6
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 23:20:04 +0100

    Add UserAuthResponseDto for user authentication details
    Introduces a new class `UserAuthResponseDto` in the
    `GrooveNest.Domain.DTOs.UserDTOs` namespace. This class
    includes properties for user authentication, such as
    `Id`, `Email`, `FirstName`, `LastName`, an optional
    `ProfilePictureUrl`, and a non-nullable `Token`.

---
+++ GrooveNest.Domain/DTOs/UserDTOs/UserAuthResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GrooveNest.Domain.DTOs.UserDTOs
+{
+    public class UserAuthResponseDto
+    {
+        public string Id { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? ProfilePictureUrl { get; set; }
+        public string Token { get; set; } = null!;
+    }
+}
