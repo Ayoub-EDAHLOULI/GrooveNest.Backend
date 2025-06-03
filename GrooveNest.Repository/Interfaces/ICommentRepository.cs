2a3ee0073d9ed9b46872cf7c09a920f353d12d9d
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-03 20:27:31 +0100

    Add ICommentRepository interface for comment management
    Introduces the `ICommentRepository` interface in the
    `GrooveNest.Repository.Interfaces` namespace. This interface
    extends `IGenericRepository` for the `Comment` entity and
    includes three asynchronous methods for retrieving and counting
    comments based on track and user identifiers.

---
+++ GrooveNest.Repository/Interfaces/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using GrooveNest.Domain.Entities;
+
+namespace GrooveNest.Repository.Interfaces
+{
+    public interface ICommentRepository : IGenericRepository<Comment, int>
+    {
+        Task<Comment?> GetByTrackIdAndUserIdAsync(Guid trackId, Guid userId);
+        Task<List<Comment>> GetCommentsByTrackIdAsync(Guid trackId);
+        Task<int> GetCommentCountByTrackIdAsync(Guid trackId);
+    }
+}
