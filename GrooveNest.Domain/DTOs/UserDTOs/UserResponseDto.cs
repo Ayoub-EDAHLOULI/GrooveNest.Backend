c89d428b4938eb947578145c6254ae337ddf358e
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-07 14:28:34 +0100

    Add Status property to UserResponseDto
    Introduced a new property `Status` of type `UserStatus`
    in the `UserResponseDto` class within the
    `GrooveNest.Domain.DTOs.UserDTOs` namespace.

--- GrooveNest.Domain/DTOs/UserDTOs/UserResponseDto.cs
+++ GrooveNest.Domain/DTOs/UserDTOs/UserResponseDto.cs
@@ -7,6 +7,7 @@ namespace GrooveNest.Domain.DTOs.UserDTOs
         public Guid Id { get; set; }
         public string UserName { get; set; } = null!;
         public string Email { get; set; } = null!;
+        public UserStatus Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
