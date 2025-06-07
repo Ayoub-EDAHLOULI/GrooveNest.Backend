4451730fac71c3606e0222e49cc06d2e139aa07f
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-07 14:29:10 +0100

    Add UserStatus to UserCreateDto
    Added a using directive for GrooveNest.Domain.Enums to
    enable the use of the UserStatus enum. Introduced a new
    Status property of type UserStatus in the UserCreateDto
    class, with a default value of UserStatus.Active,
    enhancing the data transfer object with user status information.

--- GrooveNest.Domain/DTOs/UserDTOs/UserCreateDto.cs
+++ GrooveNest.Domain/DTOs/UserDTOs/UserCreateDto.cs
@@ -1,9 +1,12 @@
-﻿namespace GrooveNest.Domain.DTOs.UserDTOs
+﻿using GrooveNest.Domain.Enums;
+
+namespace GrooveNest.Domain.DTOs.UserDTOs
 {
     public class UserCreateDto
     {
         public string UserName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+        public UserStatus Status { get; set; } = UserStatus.Active;
     }
 }
