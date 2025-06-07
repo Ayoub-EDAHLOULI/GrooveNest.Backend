b1081727d67abf34488c4c25daf1ef6de20c4df6
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-07 14:29:20 +0100

    Add Status property to UserUpdateDto
    This commit introduces a new nullable property `Status` of type `UserStatus?` to the `UserUpdateDto` class in the `UserUpdateDto.cs` file. This addition allows for the representation of the user's status, enhancing the functionality of the data transfer object.

--- GrooveNest.Domain/DTOs/UserDTOs/UserUpdateDto.cs
+++ GrooveNest.Domain/DTOs/UserDTOs/UserUpdateDto.cs
@@ -1,9 +1,12 @@
-﻿namespace GrooveNest.Domain.DTOs.UserDTOs
+﻿using GrooveNest.Domain.Enums;
+
+namespace GrooveNest.Domain.DTOs.UserDTOs
 {
     public class UserUpdateDto
     {
         public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+        public UserStatus? Status { get; set; }
     }
 }
