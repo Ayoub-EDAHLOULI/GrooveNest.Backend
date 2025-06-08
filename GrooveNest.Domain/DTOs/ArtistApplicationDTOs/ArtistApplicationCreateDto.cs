8fbb6a69143a8e620d6bf021b9326cdce85dd419
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-08 12:11:47 +0100

    Add properties to ArtistApplicationCreateDto
    Enhanced the ArtistApplicationCreateDto class by adding new properties: StageName, ArtistBio, MusicGenres, SampleTrackLinks, InstagramUrl, TwitterUrl, and YouTubeUrl. MusicGenres and SampleTrackLinks are initialized to empty lists, while the other properties are set to empty strings or nullable types.

--- GrooveNest.Domain/DTOs/ArtistApplicationDTOs/ArtistApplicationCreateDto.cs
+++ GrooveNest.Domain/DTOs/ArtistApplicationDTOs/ArtistApplicationCreateDto.cs
@@ -3,6 +3,19 @@
     public class ArtistApplicationCreateDto
     {
         public Guid UserId { get; set; }
+
+        public string StageName { get; set; } = string.Empty;
+
         public string ArtistBio { get; set; } = string.Empty;
+
+        public List<string> MusicGenres { get; set; } = [];
+
+        public List<string> SampleTrackLinks { get; set; } = [];
+
+        public string? InstagramUrl { get; set; }
+
+        public string? TwitterUrl { get; set; }
+
+        public string? YouTubeUrl { get; set; }
     }
 }
